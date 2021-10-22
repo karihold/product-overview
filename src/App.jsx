63a9e4067d39213f6ef3f3f0f517e9fafc1a09a3
karihold <holdcroftkari@gmail.com>
2021-10-22 14:10:04 +0200

    added main

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Frontpage from "pages/frontpage/frontpage";
-import Header from "landmarks/header/header";
-import Footer from "landmarks/footer/footer";
-import "scss/base.scss";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Frontpage from 'pages/frontpage/frontpage';
+import Header from 'landmarks/header/header';
+import Footer from 'landmarks/footer/footer';
+import 'scss/base.scss';
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <Frontpage />
-        </Route>
-      </Switch>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Frontpage />
+          </Route>
+        </Switch>
+      </main>
       <Footer />
     </Router>
   );
 };
 
-render(<App />, document.querySelector("#render-target"));
+render(<App />, document.querySelector('#render-target'));
