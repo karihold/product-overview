10f3e12b82093a2ba0ed52d3204554b5012e86cc
karihold <holdcroftkari@gmail.com>
2021-10-22 14:08:02 +0200

    added styling to product category

--- src/js/components/product-category/product-category.jsx
+++ src/js/components/product-category/product-category.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ProductCard from 'components/product-card/product-card';
+import './product-category.scss';
 
 const ProductCategory = ({ category, products }) => {
   return (
-    <section>
-      <h2>{category}</h2>
-      {products.map(({ title, image }) => (
-        <ProductCard title={title} type={category} image={image} />
-      ))}
+    <section className="product-category">
+      <h2 className="product-category__title">{category}</h2>
+      <div className="product-category-cards-grid">
+        {products.map(({ id, title, image, description, supported_platforms }) => (
+          <ProductCard
+            key={`product-card-${id}`}
+            id={id}
+            title={title}
+            type={category}
+            image={image}
+            description={description}
+            supportedPlatforms={supported_platforms}
+          />
+        ))}
+      </div>
     </section>
   );
 };
