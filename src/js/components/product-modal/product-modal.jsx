a78bf5e738c3a63fb6296f041bd44c0ca250d3c9
karihold <holdcroftkari@gmail.com>
2021-10-22 20:07:18 +0200

    Added product modal component

---
+++ src/js/components/product-modal/product-modal.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Modal from 'components/modal/modal';
+import Leaderboard from 'components/leaderboard/leaderboard';
+import './product-modal.scss';
+
+const ProductModal = ({ showModal, title, image, description, supportedPlatforms, productId, onClose }) => {
+  if (!showModal) return <></>;
+
+  return (
+    <Modal onClose={onClose}>
+      <section className="product-modal-wrapper">
+        <h2 className="product-modal__title">{title}</h2>
+        <p className="product-modal__description">{description}</p>
+        <section>
+          <h3 className="product-modal__supported-title">Supported Platforms:</h3>
+          <ul className="product-modal__supported-list">
+            {supportedPlatforms.map((platform) => (
+              <li className="product-modal__supported-list-item" key={`product-card-${title}-platform-${platform}`}>
+                {platform}
+              </li>
+            ))}
+          </ul>
+        </section>
+      </section>
+
+      <Leaderboard productId={productId} />
+    </Modal>
+  );
+};
+
+export default ProductModal;
