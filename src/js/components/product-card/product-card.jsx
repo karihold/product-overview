a3c473d7c2f8b62d7cc6c2c79de1528ca0830bca
karihold <holdcroftkari@gmail.com>
2021-10-22 14:08:17 +0200

    added styling to product card

--- src/js/components/product-card/product-card.jsx
+++ src/js/components/product-card/product-card.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'components/modal/modal';
+import Leaderboard from 'components/leaderboard/leaderboard';
 
-const ProductCard = ({ title, type, image }) => {
+import './product-card.scss';
+
+const ProductCard = ({ id, title, image, description, supportedPlatforms }) => {
+  const [showPortal, setShowPortal] = useState(false);
+
   return (
-    <article>
-      <div>
-        <img src={image} />
-        <p>{type}</p>
-      </div>
-      <p>{title}</p>
-    </article>
+    <>
+      <article className="product-card" onClick={() => setShowPortal((shouldShowPortal) => !shouldShowPortal)}>
+        <img className="product-card__image" src={image} />
+        <p className="product-card__title">{title}</p>
+      </article>
+      {showPortal && (
+        <Modal onClose={() => setShowPortal(false)}>
+          <h2>{title}</h2>
+          <img src={image} />
+          <p>{description}</p>
+          <ul>
+            {supportedPlatforms.map((platform) => (
+              <li key={`product-card-${title}-platform-${platform}`}>{platform}</li>
+            ))}
+          </ul>
+          <Leaderboard productId={id} />
+        </Modal>
+      )}
+    </>
   );
 };
 
