44892e7c37cf5c3b35ca5e27a07fb63975c97e7b
karihold <holdcroftkari@gmail.com>
2021-10-22 20:36:08 +0200

    Styled leaderboard users

--- src/js/components/leaderboard-user/leaderboard-user.jsx
+++ src/js/components/leaderboard-user/leaderboard-user.jsx
@@ -6,8 +6,10 @@ const LeaderboardUser = ({ username, image, score, rank }) => {
     <article className={`leaderboard-user ${rank < 4 ? 'leaderboard-user--is-top-three' : ''}`}>
       <p className="leaderboard-user__rank">{rank}</p>
       <img className="leaderboard-user__image" src={image} />
-      <p className="leaderboard-user__username">{username}</p>
-      <p className="leaderboard-user__score">{score}</p>
+      <div className="leaderboard-user__div">
+        <p className="leaderboard-user__username">{username}</p>
+        <p className="leaderboard-user__score">{score}</p>
+      </div>
     </article>
   );
 };
