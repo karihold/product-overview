ca2af1d92e460950944f7d1beaee43691725989e
karihold <holdcroftkari@gmail.com>
2021-10-22 14:07:10 +0200

    added leaderboard utilites

---
+++ src/js/utils/leaderboard.js
@@ -0,0 +1,15 @@
+import leaderboards from 'data/leaderboards.json';
+
+export function getLeaderboard(productId) {
+  const leaderboard = leaderboards.find((leaderboard) => leaderboard.product_id === productId) ?? [];
+
+  return leaderboard;
+}
+
+export function getTopThree({ entries }) {
+  return entries.slice(0, 3);
+}
+
+export function getBelowTopThree({ entries }) {
+  return entries.slice(3, entries.length - 1);
+}
