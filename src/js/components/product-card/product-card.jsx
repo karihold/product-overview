40a8a2ec2dc594680d373fd5a2d20943cd7eb030
karihold <holdcroftkari@gmail.com>
2021-10-22 20:37:11 +0200

    Added productmodal to productcard component

--- src/js/components/product-card/product-card.jsx
+++ src/js/components/product-card/product-card.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import Modal from 'components/modal/modal';
-import Leaderboard from 'components/leaderboard/leaderboard';
+import ProductModal from 'components/product-modal/product-modal';
 
 import './product-card.scss';
 
 const ProductCard = ({ id, title, image, description, supportedPlatforms }) => {
-  const [showPortal, setShowPortal] = useState(false);
+  const [showProductModal, setShowPortal] = useState(false);
 
   return (
     <>
@@ -13,19 +12,16 @@ const ProductCard = ({ id, title, image, description, supportedPlatforms }) => {
         <img className="product-card__image" src={image} />
         <p className="product-card__title">{title}</p>
       </article>
-      {showPortal && (
-        <Modal onClose={() => setShowPortal(false)}>
-          <h2>{title}</h2>
-          <img src={image} />
-          <p>{description}</p>
-          <ul>
-            {supportedPlatforms.map((platform) => (
-              <li key={`product-card-${title}-platform-${platform}`}>{platform}</li>
-            ))}
-          </ul>
-          <Leaderboard productId={id} />
-        </Modal>
-      )}
+
+      <ProductModal
+        title={title}
+        image={image}
+        description={description}
+        supportedPlatforms={supportedPlatforms}
+        productId={id}
+        onClose={() => setShowPortal(false)}
+        showModal={showProductModal}
+      />
     </>
   );
 };
