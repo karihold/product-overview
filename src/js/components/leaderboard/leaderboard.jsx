d93e5d5a86cdda09c13b2fd6de427b3010f50377
karihold <holdcroftkari@gmail.com>
2021-10-22 20:35:51 +0200

    Styled leaderboard

--- src/js/components/leaderboard/leaderboard.jsx
+++ src/js/components/leaderboard/leaderboard.jsx
@@ -6,16 +6,14 @@ import './leaderboard.scss';
 const Leaderboard = ({ productId }) => {
   const leaderboard = getLeaderboard(productId);
 
-  console.log(leaderboard);
-  console.log(leaderboard.entries);
-  console.log(leaderboard.entries.length > 0);
   if (leaderboard.entries.length === 0) return <></>;
 
   const topThree = getTopThree(leaderboard);
   const belowTopThree = getBelowTopThree(leaderboard);
 
   return (
-    <article>
+    <article className="leaderboard">
+      <h3 className="leaderboard-title">Leaderboard</h3>
       <ol className="leaderboard-top-three">
         {topThree.map(({ user_id, username, image, score, rank }) => (
           <li key={`leaderboard-user-${user_id}`}>
@@ -23,9 +21,9 @@ const Leaderboard = ({ productId }) => {
           </li>
         ))}
       </ol>
-      <ol>
+      <ol className="leaderboard-continued">
         {belowTopThree.map(({ user_id, username, image, score, rank }) => (
-          <li key={`leaderboard-user-${user_id}`}>
+          <li className="leaderboard-continued__list-item" key={`leaderboard-user-${user_id}`}>
             <LeaderboardUser username={username} image={image} score={score} rank={rank} />
           </li>
         ))}
